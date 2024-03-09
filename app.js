6964e7c31360f2739c00e39f8ac6070be5cc6b11
Othman Laafou <laafouothman0@gmail.com>
2024-03-09 17:12:40 +0100

    Add app

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ app.get("/", (req, res) => {
   res.send("Get Req Successfully initiated");
 });
 
+app.put("/put", (req, res) => {
+    console.log("PUT REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send('Data Update Request Recieved');
+  });
+
+app.post("/post", (req, res) => {
+    console.log("POST REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send("Data POST Request Recieved");
+  });
+
+app.delete("/delete", (req, res) => {
+    console.log("DELETE REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send("Data DELETE Request Recieved");
+  });
+
 app.listen(PORT, () => {
   console.log('Server established at ${PORT}');
 });
+
